c262c1d63f07ff20f003612299f1ad8bee4f08fd
AlexWang1900 <60679873+AlexWang1900@users.noreply.github.com>
2024-01-17 01:45:14 +0800

    Add files via upload

---
+++ vqgan_v2.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+from encoder import Encoder
+from decoder import Decoder
+from codebook import Codebook
+from fsq import FSQ
+
+class VQGAN2(nn.Module):
+    def __init__(self, args):
+        super(VQGAN2, self).__init__()
+        self.encoder = Encoder(args).to(device=args.device)# UNET FIRST HALF WITH ATTENTION
+        self.decoder = Decoder(args).to(device=args.device)# UNET SEC HALF WITH ATTETNION
+        self.codebook = FSQ(args.levels).to(device=args.device)
+        self.quant_conv = nn.Conv2d(args.latent_dim, len(args.levels), 1).to(device=args.device)#256,256
+        self.post_quant_conv = nn.Conv2d(len(args.levels), args.latent_dim, 1).to(device=args.device)#256,256
+
+    def forward(self, imgs):
+        encoded_images = self.encoder(imgs)#[8, 256, 8, 8])
+        quant_conv_encoded_images = self.quant_conv(encoded_images)#([8, 256, 8, 8])
+        codebook_mapping, codebook_indices = self.codebook(quant_conv_encoded_images) ##([8, 256, 8, 8])
+        q_loss = torch.tensor(0.0).cuda().float()
+        post_quant_conv_mapping = self.post_quant_conv(codebook_mapping)#([8, 256, 8, 8])
+        decoded_images = self.decoder(post_quant_conv_mapping)#([8, 3, 128, 128])
+
+        return decoded_images, codebook_indices, q_loss#([8, 3, 128, 128]),[512],0.05
+
+    def encode(self, imgs):
+        encoded_images = self.encoder(imgs)
+        quant_conv_encoded_images = self.quant_conv(encoded_images)
+        codebook_mapping, codebook_indices = self.codebook(quant_conv_encoded_images)
+        q_loss = torch.tensor(0.0).cuda().float()
+        return codebook_mapping, codebook_indices, q_loss
+
+    def decode(self, z):
+        post_quant_conv_mapping = self.post_quant_conv(z)
+        decoded_images = self.decoder(post_quant_conv_mapping)
+        return decoded_images
+
+    def calculate_lambda(self, perceptual_loss, gan_loss):
+        last_layer = self.decoder.model[-1]
+        last_layer_weight = last_layer.weight
+        perceptual_loss_grads = torch.autograd.grad(perceptual_loss, last_layer_weight, retain_graph=True)[0]
+        gan_loss_grads = torch.autograd.grad(gan_loss, last_layer_weight, retain_graph=True)[0]
+
+        λ = torch.norm(perceptual_loss_grads) / (torch.norm(gan_loss_grads) + 1e-4)
+        λ = torch.clamp(λ, 0, 1e4).detach()
+        return 0.8 * λ
+
+    @staticmethod
+    def adopt_weight(disc_factor, i, threshold, value=0.):
+        if i < threshold:
+            disc_factor = value
+        return disc_factor
+
+    def load_checkpoint(self, path):
+        self.load_state_dict(torch.load(path))
+
+
+
+
+
+
+
+
